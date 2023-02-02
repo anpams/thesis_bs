2a874cf4f04c550d0488a3d437b0390a613da5cb
anpams <anpa.eipi@gmail.com>
2023-02-03 00:12:24 +0100

    slice and audio - NOT TESTED

--- Assets/RW/Scripts/Slicer.cs
+++ Assets/RW/Scripts/Slicer.cs
@@ -1,24 +1,24 @@
 ﻿/*
  * Copyright (c) 2019 Razeware LLC
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
- * distribute, sublicense, create a derivative work, and/or sell copies of the 
- * Software in any work that is designed, intended, or marketed for pedagogical or 
- * instructional purposes related to programming, coding, application development, 
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
  * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * merger, publication, distribution, sublicensing, creation of derivative works,
  * or sale is expressly withheld.
- *    
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -38,7 +38,8 @@ public class Slicer : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // FILL IN
+        SplitMesh(other.gameObject);
+        Destroy(other.gameObject);
     }
 
     // Get a cutting plane from the rotation/position of the saber
@@ -46,7 +47,14 @@ public class Slicer : MonoBehaviour
     {
         Plane rv = new Plane();
 
-        // FILL IN
+        //Unity has function that creates plane from 3 points on the plane
+        //multiplying points by the quaternion rotation of the saber
+        Vector3 pt1 = transform.rotation * new Vector3(0,0,0);
+        Vector3 pt2 = transform.rotation * new Vector3(0,1,0);
+        Vector3 pt3 = transform.rotation * new Vector3(0,0,1);
+
+        //new plane to turn into cutting plane
+        rv.Set3Points(pt1,pt2,pt3);
 
         return rv;
     }
@@ -54,28 +62,59 @@ public class Slicer : MonoBehaviour
     // Clone a Mesh "half"
     private Mesh CloneMesh(Plane p, Mesh oMesh, bool halve)
     {
+        //
         Mesh cMesh = new Mesh();
+        cMesh.name="slicedMesh";
+        Vector3[] vertices = oMesh.vertices;
+        for (int i=0; i< vertices.Length; i++)
+        {
+        // determine with side the the vertex falls on.
+            bool side = p.GetSide(vertices[i]);
+            if(side == halve){
+                //if isn't on the side you want, map it back to the cutting plane
+                vertices[i] = p.ClosestPointOnPlane(vertices[i]);
+            }//the new mesh that appears represents only half of the object
+        }
 
-        // FILL IN
+        //reassings cloned vertices, triangles, normals and UV
+        //returns mesh as half.
+        cMesh.vertices= vertices;
+        cMesh.triangles = oMesh.triangles;
+        cMesh.normals = oMesh.normals;
+        cMesh.uv = oMesh.uv; //uv -> texture coordinates. 2d position within a texture
 
         return cMesh;
     }
 
     // Configure the GameObject
-    private GameObject MakeHalf(GameObject go, bool isLeft)
+    GameObject MakeHalf(GameObject go, bool isLeft)
     {
-        // 1.
+
         float sign = isLeft ? -1 : 1;
-        GameObject half = Instantiate(go);
+        GameObject half = Instantiate(go); //create copy with instantiate
+        MeshFilter filter = half.GetComponent<MeshFilter>();
 
-        // FILL IN
+        Plane cuttingPlane = GetPlane(go);// Determines the cutting plane
+        filter.mesh = CloneMesh(cuttingPlane, filter.mesh, isLeft); //clones the mesh tusing the cutting plane as slice
 
-        return half;
+        half.transform.position = go.transform.position + transform.rotation * new Vector3(sign * 0.05f, 0, 0);
+        half.GetComponent<Rigidbody>().isKinematic = false; //reenable physics by disabling isKinematic
+        half.GetComponent<Rigidbody>().useGravity = true; //and enabling useGravity
+        half.GetComponent<Collider>().isTrigger = false; //disable further subslicing (to prevent rapid cloning of subslices)
+
+        Destroy(half, 2);
+    	return half;
     }
+
 
     // Make two GameObjects with "halves" of the original
     private void SplitMesh(GameObject go)
     {
-        // FILL IN
-    }
+        GameObject leftHalf= MakeHalf(go, true); //makes two copies of the mesh for the illusion
+        GameObject righHalf= MakeHalf(go, false);
+
+        GetComponent<AudioSource>().Play(); //hit sound
+     }
+
+
 }
